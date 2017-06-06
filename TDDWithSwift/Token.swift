43ee6536af8649b55331f034fa410ae729d32d25
Andrew Tantomo <Andrew@Andrews-MacBook-Pro.local>
2017-06-07 07:27:37 +0900

    chapter 5 completed

---
+++ TDDWithSwift/Token.swift
@@ -0,0 +1,15 @@
+//
+//  Token.swift
+//  TDDWithSwift
+//
+//  Created by Andrew Tantomo on 2017/05/28.
+//  Copyright © 2017年 Andrew Tantomo. All rights reserved.
+//
+
+import Foundation
+
+struct Token {
+
+    let id: String 
+
+}
